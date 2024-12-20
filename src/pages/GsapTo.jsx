3057d2796187a3d02ea1974daadd6400f05bcebe
Jessica Kiroff <jesskiroff@gmail.com>
2024-12-20 15:09:00 -0500

    import gsap

--- src/pages/GsapTo.jsx
+++ src/pages/GsapTo.jsx
@@ -1,6 +1,9 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
 const GsapTo = () => {
   // TODO: Implement the gsap.to() method
-
+  useGSAP;
   return (
     <main>
       <h1>GsapTo</h1>
@@ -10,22 +13,22 @@ const GsapTo = () => {
         current state to a new state.
       </p>
       <p className="mt-5 text-gray-500">
-        The <code>gsap.to()</code> method is similar to the{" "}
-        <code>gsap.from()</code> method, but the difference is that the{" "}
+        The <code>gsap.to()</code> method is similar to the{' '}
+        <code>gsap.from()</code> method, but the difference is that the{' '}
         <code>gsap.to()</code> method animates elements from their current state
         to a new state, while the <code>gsap.from()</code> method animates
         elements from a new state to their current state.
       </p>
 
       <p className="mt-5 text-gray-500">
-        Read more about the{" "}
+        Read more about the{' '}
         <a
           href="https://greensock.com/docs/v3/GSAP/gsap.to()"
           target="_blank"
           rel="noreferrer noopener nofollow"
         >
           gsap.to()
-        </a>{" "}
+        </a>{' '}
         method.
       </p>
 
